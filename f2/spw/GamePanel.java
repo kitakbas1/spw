4ab41d2e864894df0af9081cadc60f448fe81876
kitakbas1 <sithinon.9@hotmail.com>
2015-05-06 16:47:32 +0700

    *05*
    1.เพิ่มไฟล์ภาพ
    2.เปลี่ยนพื้นหลังเป็นรูปภาพที่เหมาะกับเกม

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -8,20 +8,37 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
+import java.io.IOException;    //try catch
+import java.awt.Image;        //picture
+import javax.imageio.ImageIO;  
+import java.io.File;
+
 public class GamePanel extends JPanel {
 	
 	private BufferedImage bi;	
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
+	private Image pic1;
 
 	public GamePanel() {
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.BLUE);
+		//big.setBackground(Color.BLUE);
+		
+		try {
+			
+			pic1 = ImageIO.read(new File("C:\\Users\\Sithinon\\Documents\\spw\\f2\\img\\piv.jpg"));
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
-
+		
+	
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
+		big.drawImage(pic1,0, 0,600,650,this);
 		big.setColor(Color.RED);
 	    big.drawString(String.format("%08d", reporter.getScore()), 320, 20);
 		big.drawString(String.format("Level = %d", reporter.getLevel()), 20, 20);
