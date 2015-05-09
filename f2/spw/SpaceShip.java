d18ee9836babe192a91a041c7bd6eec44d11d9bb
kitakbas1 <sithinon.9@hotmail.com>
2015-05-10 06:59:19 +0700

    *06*(2)
    upload เพิ่ม

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -2,21 +2,28 @@ package f2.spw;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.io.IOException;    //try catch
+import java.awt.Image;        //picture
+import javax.imageio.ImageIO;  
+import java.io.File;
 
 public class SpaceShip extends Sprite{
 
 	int step = 8;
-	
+	private Image space;
 	public SpaceShip(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		
+		try{
+               space = ImageIO.read(new File("C:\\Users\\Sithinon\\Documents\\spw\\f2\\img\\piv3.png"));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(x, y, width, height);
-		
+		g.drawImage(space ,x ,y ,width+15,height+10,null);	
 	}
 
 	public void move(int direction){
